9f8afb466e73543479e839b173871b629f6ef86a
iti <itishree1999@gmail.com>
2020-04-02 20:39:28 +0530

    Indenting code

--- MatrixMultiplication/MatrixMultiplication.cpp
+++ MatrixMultiplication/MatrixMultiplication.cpp
@@ -24,74 +24,77 @@ int hpx_main(hpx::program_options::variables_map& vm)
 	uint64_t cols = vm["cols"].as<std::size_t>();
 
 	
-	vec A(rows*rows);
-	vec B(cols*cols);
-    std::cout<<"Matrix A is : \n";
+	vec A(rows * rows);
+	vec B(cols * cols);
+
+	std::cout << "Matrix A is : \n";
+	size_t n = rows;
 	for (std::size_t i = 0; i < rows; i++)
 	{
 		for (std::size_t j = 0; j < rows; j++)
 		{
-			A[i+j] = rand() % 20 + 1;
-			B[i+j] = rand() % 20 + 1;
-			std::cout <<A[i+j]<<" ";
+			A[i * n + j] = rand() % 20 + 1;
+			B[i * n + j] = rand() % 20 + 1;
+			std::cout << A[i*n+j] << " ";
 		}
-		std::cout <<"\n"; 
+		std::cout << "\n"; 
 	}
-	std::cout<<"Matrix B is : \n";
+
+	std::cout << "Matrix B is : \n";
 	for (std::size_t i = 0; i < rows; i++)
 	{
 		for (std::size_t j = 0; j < rows; j++)
 		{
-			std::cout<<B[i+j]<<" ";
+			std::cout<<B[i * n + j]<<" ";
 		}
 		std::cout <<"\n";
 	}
 
-	size_t n = rows;
-	vec res(n*n);
 
+	vec res(n * n);
 
 	using hpx::parallel::for_each;
 	using hpx::parallel::execution::par;
- 	auto range = boost::irange<std::size_t>(0, n);
+	auto range = boost::irange<std::size_t>(0, n);
 	for_each(par, boost::begin(range), boost::end(range),
-	[&](std::size_t i)
-	{
-          for(std::size_t j = 0; j < n; j++)
-             {
-             	 res[i*n+j] = 0;
-             	for (std::size_t k = 0; k < n; k++)
-                 {
-                 	res[i*n + j] += A[i*n+k]*B[k*n+j];
-                 }  
-             }
+		[&](std::size_t i)
+		{
+			for(std::size_t j = 0; j < n; j++)
+			{
+				res[i * n + j] = 0;
+				for (std::size_t k = 0; k < n; k++)
+				{
+					res[i * n + j] += A[i * n + k] * B[k * n + j];
+				}  
+			}
 
-    }
-    );
-	std::cout <<n<<"\n";
-	std::cout<<"Resultant Matrix is : \n";
-    for (std:: size_t i = 0; i < n; i++)
-    {
-    	for (std:: size_t j = 0; j < n; j++)
-    		std::cout <<res[i*n +j]<<" ";
-    	std::cout <<"\n";
-    }
-    return hpx::finalize();
+		}
+		);
+
+	std::cout << n << "\n";
+	std::cout << "Resultant Matrix is : \n";
+	for (std:: size_t i = 0; i < n; i++)
+	{
+		for (std:: size_t j = 0; j < n; j++)
+			std::cout << res[i * n + j] <<" ";
+		std::cout <<"\n";
+	}
+	return hpx::finalize();
 }
 int main(int argc, char* argv[])
 {
-	 hpx::program_options::options_description
-       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+	hpx::program_options::options_description
+	desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
 
-    desc_commandline.add_options()
-    ( "rows",
-          hpx::program_options::value<std::uint64_t>()->default_value(2),
-          "Number of rows")
-    ("cols", 
-    	  hpx::program_options::value<std::uint64_t>()->default_value(2),
-          "Number of cols")
-    ;
+	desc_commandline.add_options()
+	( "rows",
+		hpx::program_options::value<std::uint64_t>()->default_value(4),
+		"Number of rows")
+	("cols", 
+		hpx::program_options::value<std::uint64_t>()->default_value(4),
+		"Number of cols")
+	;
 
     // Initialize and run HPX
-    return hpx::init(desc_commandline, argc, argv);
+	return hpx::init(desc_commandline, argc, argv);
 }
